4a6b67f412c409f09e58beb113d2bdc3a0a59013
maskho <maskhobar@gmail.com>
2023-04-01 01:06:21 +0700

    add login

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,69 @@
+import { SyncOutlined } from "@ant-design/icons";
+import axios from "axios";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/login`, {
+        email,
+        password,
+      });
+      console.log("LOGIN RESPONSE", data);
+      //   setLoading(false);
+    } catch (error) {
+      toast.error(error.response.data);
+      setLoading(false);
+    }
+  };
   return (
     <>
       <div className="p-5 mb-4 bg-primary bg-gradient rounded-0">
         <div className="container-fluid py-5">
           <h1 className="display-5 fw-bold text-center">Login Page</h1>
         </div>
+      </div>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+          <div className="d-grid gap-2">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={!email || !password || loading}
+            >
+              {loading ? <SyncOutlined spin /> : "Submit"}
+            </button>
+          </div>
+        </form>
+        <p className="text-center p-3">
+          Not yet registered?
+          <Link href={"/register"}> Register</Link>
+        </p>
       </div>
     </>
   );
